46e8fe231769c229146dfd03e368a83dbdaad0cf
George Stavrinos <gstavrinos@protonmail.com>
2023-06-08 20:27:58 +0300

    Added missing dependency

--- install_dependencies/install_without_environment.sh
+++ install_dependencies/install_without_environment.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-sudo apt install ros-melodic-teleop-twist-keyboard python-sklearn ffmpeg
+sudo apt install ros-melodic-teleop-twist-keyboard python-tk python-sklearn ffmpeg
 pip install -r requirements.txt
