6abbf7e33010d7ad3c442d8b747c5ec6368c56d0
StavrouNikolas <116565556+StavrouNi@users.noreply.github.com>
2023-09-05 23:53:37 +0300

    Update sac_discrete_agent.py

--- scripts/sac_discrete_agent.py
+++ scripts/sac_discrete_agent.py
@@ -1,7 +1,7 @@
 import rospy
 import torch
 import numpy as np
-from networks_discrete import update_params, Actor, Critic, ReplayBuffer
+from networks_discrete import update_params, Actor, Critic, ReplayBuffer, Dual_ReplayBuffer
 import torch.nn.functional as F
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -31,6 +31,10 @@ class DiscreteSACAgent:
       
         self.actor = Actor(self.input_dims, self.n_actions, self.layer1_size, chkpt_dir=self.chkpt_dir).to(device)
         self.critic = Critic(self.input_dims, self.n_actions, self.layer1_size, chkpt_dir=self.chkpt_dir).to(device)
+        # The above 3 lines are for the LfD participants gameplay to initialize the dual buffer
+        self.demo_data = rospy.get_param("rl_control/Game/load_demonstrations_data_dir","opt/ros/catkin_ws/src/hrc_study_tsitosetal/buffers/demo_buffer.npy")
+        self.lfd_transfer_gameplay = rospy.get_param('rl_control/Game/lfd_transfer_gameplay', False)
+        self.percentages = [1.0, 0.8, 0.6, 0.3, 0.1]
         self.target_critic = Critic(self.input_dims, self.n_actions, self.layer1_size, chkpt_dir=self.chkpt_dir).to(
             device)
 
@@ -53,11 +57,17 @@ class DiscreteSACAgent:
         self.log_alpha = torch.zeros(1, requires_grad=True, device=device)
         self.alpha_optim = torch.optim.Adam([self.log_alpha], lr=self.lr, eps=1e-4)
 
-        self.memory = ReplayBuffer(self.buffer_max_size)
-
-    def learn(self, interaction=None):
+        if self.lfd_transfer_gameplay:
+            self.memory= Dual_ReplayBuffer(self.buffer_max_size,self.demo_data,self.percentages)
+        else:
+            self.memory = ReplayBuffer(self.buffer_max_size)
+            
+    def learn(self,episode_number, interaction=None):
         if interaction is None:
-            states, actions, rewards, states_, dones = self.memory.sample(self.batch_size)
+            if self.lfd_transfer_gameplay:
+                states, actions, rewards, states_, dones= self.memory.sample(self.batch_size,episode_number)
+            else:
+                states, actions, rewards, states_, dones = self.memory.sample(self.batch_size)
         else:
             states, actions, rewards, states_, dones = interaction
             states, actions, rewards, states_, dones = [np.asarray([states]), np.asarray([actions]),
