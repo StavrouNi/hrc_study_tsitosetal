ab92f75e09765ee9cabdf16b37fdff03052f0920
StavrouNi <your.email@example.com>
2023-10-24 15:25:36 +0300

    another expert

--- scripts/ExperttoDemoBufferTestFunc.py
+++ scripts/ExperttoDemoBufferTestFunc.py
@@ -8,6 +8,8 @@ def ExpertoDemo(filename, save_path):
 
     last_column = data[:, -1]
     true_count = np.sum(last_column)
+    total_games = np.sum(data[:, -1])
+    print("Total number of games:", total_games)
 
     indices = np.where(last_column)[0]
     splits = np.array_split(indices, len(indices) // 10)
@@ -20,6 +22,7 @@ def ExpertoDemo(filename, save_path):
         end_idx = split[-1] + 1
         split_data = data[start_idx:end_idx]
         episodes.append(split_data)
+        print("Total games in episode {}: {}".format(len(episodes), len(split_data)))  # Added this line
 
     extracted_games = []
 
@@ -44,14 +47,14 @@ def ExpertoDemo(filename, save_path):
         extracted_games.extend(winning_games[:num_extracted_games])
 
 
-
+    print(extracted_games)
     for game in extracted_games:
         print("Game shape:", game.shape)
 
-    if episodes:
-        print("Second episode:", episodes[1])
-    if extracted_games:
-        print("First game of extracted winning games:", extracted_games[0])
+    #if episodes:
+        #print("Second episode:", episodes[1])
+    #if extracted_games:
+        #print("First game of extracted winning games:", extracted_games[0])
     print("Number of extracted winning games total:", len(extracted_games))
 
     # Save the demogames to an array and to a specific path
@@ -66,13 +69,18 @@ def ExpertoDemo(filename, save_path):
             extracted_games = extracted_games[:num_to_keep]
     with open(save_path, 'wb') as f:
         pickle.dump(demo_games, f, protocol=2)
+    print("Shortest games:")
+    for i, game in enumerate(extracted_games[:5]):
+        print("Game {} shape:".format(i+1), game.shape)
+        print(game)
+        print("-" * 30)
 
     print("shape", demo_games.shape)
-    print(type(demo_games))
-    print("Number of extracted winning games total:", len(demo_games))  # Print the length of demo_games
-
+    #print(type(demo_games))
+   # print("Number of extracted winning games total:", len(demo_games))  # Print the length of demo_games
+    #print("Total games overall: {}".format(len(data)))
     return demo_games
-save_path='/home/ttsitos/catkin_ws/src/hrc_study_tsitosetal/buffers/demo_buffer/final_demo_buffer.npy'
+save_path='/home/ttsitos/catkin_ws/src/hrc_study_tsitosetal/buffers/demo_buffer/DELETE.npy'
 
-filename = '/home/ttsitos/catkin_ws/src/hrc_study_tsitosetal/buffers/expert_buffer/buffer_data_2.npy'
+filename = '/home/ttsitos/catkin_ws/src/hrc_study_tsitosetal/buffers/expert_buffer/buffer_data_3.npy'
 ExpertoDemo(filename, save_path)
